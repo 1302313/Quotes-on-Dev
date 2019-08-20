a0ab8cc69da68e395eaea1e831e4712e23ef0885
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-08-20 12:02:11 -0700

    Init

---
+++ js/qod.js
@@ -0,0 +1,13 @@
+(function($) {
+  $(function() {
+    $body = $('body');
+
+    $body.append('test');
+
+    //your ajax and other JS code
+
+    // 1: Get Request for wp/v2/posts
+
+    // 2: Get Request for wp/v2/posts
+  });
+})(jQuery);
