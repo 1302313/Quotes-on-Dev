9aad772b814b93fcf991a8a8016978194073bfc8
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-08-22 22:06:34 -0700

    Add about page

--- about.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Template part for displaying about page content.php.
- *
- * @package QOD_Starter_Theme
- */
---
+++ page-about.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Template part for displaying about page content.php.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+
+        <?php while (have_posts()) : the_post(); ?>
+
+        <h2> <?php the_title() ?> </h2>
+
+        <div class="about-info">
+            <p>Quotes on Dev is a project site for the RED Academy Web Developer Professional program. It’s used to experiment with Ajax, WP API, jQuery, and other cool things. 🙂</p>
+
+        </div>
+
+
+
+        <?php endwhile; ?>
+    </main>
+</div>
+
+<?php get_footer(); ?>
