8e0fd04c9cbffe2b56b89e9036b59efc535c68e8
AdhamElNewihy <adham.el.newihy@gmail.com>
2019-08-25 16:28:03 -0700

    Initial design of form page complete

--- page-submit.php
+++ page-submit.php
@@ -8,42 +8,45 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="page-submit">
     <main id="main" class="site-main" role="main">
 
         <?php while (have_posts()) : the_post(); ?>
 
+        <!-- Submit Page Title -->
+        <h1> <?php the_title() ?> </h1>
+
         <section class="quote-submission">
 
-            <header class="entry-header page-submit-header">
+            <!-- <header class="entry-header page-submit-header">
                 <h1 class="page-submit-header-title"></h1>
-            </header>
+            </header> -->
 
             <?php if (is_user_logged_in() && current_user_can('edit_posts')) : ?>
-            <div class="page-submit-submission-wrapper">
+            <div class="page-submit-wrapper">
                 <form id="quote-submission-form" name="quoteForm">
 
-                    <div>
+                    <div class="quote-submission-form-content">
                         <label for="quote-author">Author of Quote</label>
                         <input type="text" name="quoteAuthor" id="quote-author">
                     </div>
 
-                    <div>
+                    <div class="quote-submission-form-content">
                         <label for="quote-content">Quote</label>
                         <textarea name="quoteName" id="quote-content" cols="20" rows="3" required></textarea>
                     </div>
 
-                    <div>
+                    <div class="quote-submission-form-content">
                         <label for="quote-source">Where did you find this Quote? (E.g. Book or Journal Name)</label>
                         <input type="text" name="quoteSource" id="quote-source">
                     </div>
 
-                    <div>
+                    <div class="quote-submission-form-content">
                         <label for="quote-source-url">Provide the link to the Quote source</label>
                         <input type="url" name="quoteSourceUrl" id="quote-source-url">
                     </div>
 
-                    <input type="submit" value="Submit Quote">
+                    <input class="submit-button" type="submit" value="Submit Quote">
 
                 </form>
             </div>
